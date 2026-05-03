b066208cd1d688d289e3f106c8081c9362a70384
Laura Lisca <lauralisgo@unisabana.edu.co>
2026-05-03 16:25:58 -0500

    lab 9

--- car.py
+++ car.py
@@ -1,19 +1,19 @@
 class Car:
-    def __init__(self, car_id, brand, year, color, milage=0.0):
+    def __init__(self, car_id, brand, year, color, mileage=0.0):
         self.car_id = car_id
         self.brand = brand
         self.year = year
         self.color = color
-        self.milage = milage
+        self.mileage = mileage
 
     def change_color(self, new_color):
         self.color = new_color
 
     def drive(self, miles):
-        self.milage += miles 
+        self.mileage += miles 
 
     def __str__(self):
-        return f"{self.car_id} - {self.year} {self.color} {self.brand} with {self.milage} miles"
+        return f"{self.car_id} - {self.year} {self.color} {self.brand} with {self.mileage} miles"
         
 
 
