c98542555206b3b2f47e58e4f8c96055fd22db3b
Laura Lisca <lauralisgo@unisabana.edu.co>
2026-05-03 15:44:42 -0500

    lab 9

--- car.py
+++ car.py
@@ -1,2 +1,20 @@
-# Write your class definition here!
+class Car:
+    def __init__(self, car_id, brand, year, color, milage=0.0):
+        self.car_id = car_id
+        self.brand = brand
+        self.year = year
+        self.color = color
+        self.milage = milage
+
+    def change_color(self, new_color):
+        self.new_color = new_color
+
+    def drive(self, miles):
+        self.miles = miles 
+
+    def __str__(self):
+        return f"{self.car_id} - {self.year} {self.color} {self.brand} with {self.milage} miles"
+        
+
+
 
